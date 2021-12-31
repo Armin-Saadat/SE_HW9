76ec94b82f84edf9bda984ea0561f50c74d02df3
rmool <armin2saadat@gmail.com>
2022-01-01 00:37:12 +0330

    refactor control statement braces and delete useless commented codes.

--- src/main/java/semantic/symbol/SymbolTable.java
+++ src/main/java/semantic/symbol/SymbolTable.java
@@ -53,14 +53,10 @@ public class SymbolTable {
     }
 
     public void addMethodLocalVariable(String className, String methodName, String localVariableName) {
-//        try {
-            if (klasses.get(className).Methodes.get(methodName).localVariable.containsKey(localVariableName)) {
-                ErrorHandler.printError("This variable already defined");
-            }
-            klasses.get(className).Methodes.get(methodName).localVariable.put(localVariableName, new Symbol(lastType, mem.getDateAddress()));
-//        }catch (NullPointerException e){
-//            e.printStackTrace();
-//        }
+        if (klasses.get(className).Methodes.get(methodName).localVariable.containsKey(localVariableName)) {
+            ErrorHandler.printError("This variable already defined");
+        }
+        klasses.get(className).Methodes.get(methodName).localVariable.put(localVariableName, new Symbol(lastType, mem.getDateAddress()));
     }
 
     public void setSuperClass(String superClass, String className) {
@@ -71,20 +67,15 @@ public class SymbolTable {
         return keyWords.get(keywordName);
     }
 
-    public Symbol get(String fieldName,String className) {
-//        try {
-            return klasses.get(className).getField(fieldName);
-//        }catch (NullPointerException n)
-//        {
-//            n.printStackTrace();
-//            return null;
-//        }
+    public Symbol get(String fieldName, String className) {
+        return klasses.get(className).getField(fieldName);
     }
 
     public Symbol get(String className, String methodName, String variable) {
         Symbol res = klasses.get(className).Methodes.get(methodName).getVariable(variable);
-        if (res == null)
+        if (res == null) {
             res = get(variable, className);
+        }
         return res;
     }
 
@@ -92,13 +83,8 @@ public class SymbolTable {
         return klasses.get(className).Methodes.get(methodName).getNextParameter();
     }
 
-    public void startCall(String className,String methodName) {
-//        try {
-            klasses.get(className).Methodes.get(methodName).reset();
-//        }catch (NullPointerException n)
-//        {
-//            n.printStackTrace();
-//        }
+    public void startCall(String className, String methodName) {
+        klasses.get(className).Methodes.get(methodName).reset();
     }
 
     public int getMethodCallerAddress(String className, String methodName) {
@@ -110,13 +96,7 @@ public class SymbolTable {
     }
 
     public SymbolType getMethodReturnType(String className, String methodName) {
-//        try {
-            return klasses.get(className).Methodes.get(methodName).returnType;
-//        }catch (NullPointerException ed){
-//            ed.printStackTrace();
-//            return null;
-//        }
-
+        return klasses.get(className).Methodes.get(methodName).returnType;
     }
 
     public int getMethodAddress(String className, String methodName) {
@@ -139,7 +119,6 @@ public class SymbolTable {
                 return Fields.get(fieldName);
             }
             return superClass.getField(fieldName);
-
         }
 
     }
@@ -165,10 +144,12 @@ public class SymbolTable {
         }
 
         public Symbol getVariable(String variableName) {
-            if (parameters.containsKey(variableName))
+            if (parameters.containsKey(variableName)) {
                 return parameters.get(variableName);
-            if (localVariable.containsKey(variableName))
+            }
+            if (localVariable.containsKey(variableName)) {
                 return localVariable.get(variableName);
+            }
             return null;
         }
 
@@ -187,17 +168,3 @@ public class SymbolTable {
     }
 
 }
-
-//class Symbol{
-//    public SymbolType type;
-//    public int address;
-//    public Symbol(SymbolType type , int address)
-//    {
-//        this.type = type;
-//        this.address = address;
-//    }
-//}
-//enum SymbolType{
-//    Int,
-//    Bool
-//}
